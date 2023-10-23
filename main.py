8d5258336455f9b45868a8e1f10b4cfddc1f8da0
Thierry Vinicius <thierry.vinicius@outlook.com>
2023-10-23 16:28:31 -0400

    Criando o controle do Cliente e fazendo o teste de conexão e cadastro no banco de dados

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#initial commit.
+from modelo.conexao import Conexao
+from modelo.cliente import Cliente
+from controle.clienteC import ClienteC
+
+# Configuração da conexão com o banco de dados
+
+conexao = Conexao('localhost', 'database', 'usuario', 'senha')
+cadastro = ClienteC(conexao)
+cadastro.cadastrar_clientes('usuario','numero','1')
+print(cadastro.consultar_clientes())
+conexao.fechar_conexao()
+
--- modelo/cliente.py
+++ modelo/cliente.py
@@ -1,6 +1,11 @@
-class Pessoa:
+class Cliente:
     def __init__(self,nome,telefone,codigo):
         self.nome = nome
         self.telefone = telefone
         self.codigo = codigo
+
+    def cadastrar_cliente(self):
+        nome = input('Digite o nome do cliente: ')
+        telefone = input('Digite o telefone do cliente: ')
+        cod = input('Digite o codigo do cliente: ')
 
